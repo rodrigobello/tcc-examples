8af41f53584cb5bf1c75bdda82c1d022ac540903
Rodrigo Bello <rodrigoabbello@gmail.com>
2020-09-29 18:27:40 -0300

    Change indentation to 4 spaces

--- CancelProcessUseCase.java
+++ CancelProcessUseCase.java
@@ -1,13 +1,13 @@
 public class CancelProcessUseCase {
-   private IProcessRepository processRepository;
+    private IProcessRepository processRepository;
 
-   public CancelProcessUseCase(IProcessRepository processRepository) {
-      this.processRepository = processRepository;
-   }
+    public CancelProcessUseCase(IProcessRepository processRepository) {
+        this.processRepository = processRepository;
+    }
 
-   public void execute(int processId, String reason) {
-      Process process = this.processRepository.getProcessById(processId);
-      process.cancel(reason);
-      this.processRepository.save(process)
-   }
+    public void execute(int processId, String reason) {
+        Process process = this.processRepository.getProcessById(processId);
+        process.cancel(reason);
+        this.processRepository.save(process)
+    }
 }
