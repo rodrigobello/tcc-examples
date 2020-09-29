fd7446d679bc6877d4e87ccf99648885ae1cab9f
Rodrigo Bello <rodrigoabbello@gmail.com>
2020-09-29 14:57:49 -0300

    Change indentation tab to 4 spaces

--- Process.java
+++ Process.java
@@ -1,13 +1,13 @@
 public class Process {
-   private Status status;
-   private String canceledReason;
-   private User canceledBy;
+    private Status status;
+    private String canceledReason;
+    private User canceledBy;
 
-   // process logic here...
+    // process logic here...
 
-   public void cancel(String reason, User user) {
-      this.status = Status.CANCELED;
-      this.canceledReason = reason;
-      this.canceledBy = user;
-   }
+    public void cancel(String reason, User user) {
+        this.status = Status.CANCELED;
+        this.canceledReason = reason;
+        this.canceledBy = user;
+    }
 }
