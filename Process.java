95b77a82982bf01a9ab219327ddd2806ed78100f
Rodrigo Bello <rodrigoabbello@gmail.com>
2020-09-29 14:56:38 -0300

    Allow to cancel an ongoing process

--- Process.java
+++ Process.java
@@ -1,3 +1,11 @@
 public class Process {
+   private Status status;
+   private String canceledReason;
+
    // process logic here...
+
+   public void cancel(String reason) {
+      this.status = Status.CANCELED;
+      this.canceledReason = reason;
+   }
 }
