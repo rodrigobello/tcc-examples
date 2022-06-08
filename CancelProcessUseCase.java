bddf9ca06cac986bee82899e6021be4370fe5789
Rodrigo Bello <rodrigobello@gmail.com>
2022-06-08 19:09:16 -0300

    Kick off process events after canceling it

--- CancelProcessUseCase.java
+++ CancelProcessUseCase.java
@@ -1,13 +1,16 @@
 public class CancelProcessUseCase {
-   private IProcessRepository processRepository;
+    private IProcessRepository processRepository;
+    private IPublisher publisher;
 
-   public CancelProcessUseCase(IProcessRepository processRepository) {
-      this.processRepository = processRepository;
-   }
+    public CancelProcessUseCase(IProcessRepository processRepository, IPublisher publisher) {
+        this.processRepository = processRepository;
+        this.publisher = publisher;
+    }
 
-   public void execute(int processId, String reason, User user) {
-      Process process = this.processRepository.getProcessById(processId);
-      process.cancel(reason, user);
-      this.processRepository.save(process);
-   }
+    public void execute(int processId, String reason, User user) {
+        Process process = this.processRepository.getProcessById(processId);
+        process.cancel(reason, user);
+        this.processRepository.save(process);
+        this.publisher.publish(process.getEvents());
+    }
 }
