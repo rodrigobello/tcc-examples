5f08570947bac74a784059d03b8f2ab584f85ef7
Rodrigo Bello <rodrigobello@gmail.com>
2022-06-08 19:04:19 -0300

    Create use case for canceling a process

---
+++ CancelProcessUseCase.java
@@ -0,0 +1,13 @@
+public class CancelProcessUseCase {
+   private IProcessRepository processRepository;
+
+   public CancelProcessUseCase(IProcessRepository processRepository) {
+      this.processRepository = processRepository;
+   }
+
+   public void execute(int processId, String reason) {
+      Process process = this.processRepository.getProcessById(processId);        
+      process.cancel(reason);
+      this.processRepository.save(process);
+   }
+}
